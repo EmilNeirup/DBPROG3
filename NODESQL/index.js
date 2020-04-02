db3cd79992d90df833dc84c89c482acbdd8e9c77
Emil Neirup Jensen <enj@addition.dk>
2020-04-02 09:36:34 +0200

    Added show teasks, and create person

--- NODESQL/index.js
+++ NODESQL/index.js
@@ -13,9 +13,7 @@ app.get('/', function (req, res){
     res.sendFile(path.join(__dirname + '/index2.html'));
 });
 
-app.get('/', function (req, res){
-    res.sendFile(path.join(__dirname + '/created.html'));
-});
+
 
 
 //Tilgår driverne
@@ -60,7 +58,7 @@ app.post('/employees', function (req, res) {
     });
 });
 
-app.post('/created', function (req) {
+app.post('/created-person', function (req, res) {
     
     let creatPersonId=req.body.inputId;
     console.log(creatPersonId);
@@ -76,7 +74,10 @@ app.post('/created', function (req) {
         sqlRequest.query(sqlQuery,sql, function(err, data){
             if (err) console.log(err) 
 
-        
+        let head=' <head><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></head>'  
+        let h='<div class="container"> <h1>' + creatPersonNavn + 'oprettet</h1>'
+        res.send(head+h)
+
         sql.close();
         });
     });
@@ -99,6 +100,36 @@ app.post('/created', function (req) {
             if (err) console.log(err) 
 
         
+        sql.close();
+        });
+    });
+});
+
+app.post('/tasks', function (req, res) {
+    sql.connect(config, function(err) {
+        if (err) console.log(err);
+
+        let sqlRequest = new sql.Request();
+
+        let taskId=req.body.taskSelected;
+        console.log(taskId + 'Ja det her er task id');
+
+        let sqlQuery='SELECT Personer.navn AS personNavn, Personer.id, Tasks.navn AS taskNavn, Tasks.timeStart, Tasks.timeStop FROM Personer INNER JOIN PersonerToTasks ON PersonerToTasks.personerId = Personer.id INNER JOIN Tasks ON PersonerToTasks.tasksId = Tasks.id WHERE Tasks.id = ' + parseInt(taskId);
+        sqlRequest.query(sqlQuery, function(err, data){
+            if (err) console.log(err)
+
+        let head=' <head><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></head>'  
+        let h='<div class="container"> <h1>' + data.recordset[0].taskNavn + 's detaljer</h1>'
+        let str='<table class="table">';
+        let row='';
+        for (let j=0;j<data.recordset.length;j++){
+            row=row + '<tr>' + '<td>' + data.recordset[j].id +'</td>' + '<td>' + data.recordset[j].personNavn +'</td>' + '<td>' + data.recordset[j].timeStart +'</td>'+ '<td>' + data.recordset[j].timeStop +'</td>'
+        }
+         str=str + row + '</table>';
+        let bottom ='</div>'
+        res.send(head+h+str+bottom)
+
+
         sql.close();
         });
     });
